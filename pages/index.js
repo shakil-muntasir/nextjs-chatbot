86d54e31cacb2f17e7cb98ace5f69d2600355654
Muntasir Rahman <shakil_muntasir@hotmail.com>
2023-08-03 22:07:06 +0600

    fix height

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import ChatWindow from '@/components/ChatWindow'
 export default function Home() {
   return (
     <main className={`flex justify-center bg-blue-400/25 sm:bg-white`}>
-      <div className='flex min-h-screen flex-col items-center justify-center sm:w-[400px] sm:bg-blue-400/25'>
+      <div className='flex h-screen flex-col items-center justify-center sm:w-[400px] sm:bg-blue-400/25'>
         <ChatWindow />
       </div>
     </main>
